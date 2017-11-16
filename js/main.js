22142fbc51ce52e084cdeb1bb3151d2f51f6422c
chriswillphoto <christophernekich@gmail.com>
2017-11-16 21:14:37 +1100

    light switch i made in 20 minutes just for kicks

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let on = true;
+
+$(document).ready(function(){
+
+  $(document).keydown(function(event){
+    if(event.keyCode === 32 ){
+      if(on){
+        $("body").css("filter", "brightness(0.4)");
+        $("body").css("background-color", "black");
+        $(".switch").animate({ top: "5%"}, 250);
+        $(".switch2").animate({ top: "5%"}, 250);
+
+        on = false;
+      }else{
+        $("body").css("filter", "brightness(1)");
+        $("body").css("background-color", "grey");
+        $(".switch").animate({ top: "70%"}, 250);
+        $(".switch2").animate({ top: "15%"}, 250);
+        on = true
+      };
+    };
+  });
+
+
+
+
+});
